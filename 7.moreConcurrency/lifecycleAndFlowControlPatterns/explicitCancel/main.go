d48966d501bee66f4e99432ab8ec3660ec42ce0c
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2026-05-13 21:35:32 +0300

    explicit cancellation / done channel

---
+++ 7.moreConcurrency/lifecycleAndFlowControlPatterns/explicitCancel/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	done := make(chan struct{})
+	defer close(done)
+
+	in := gen(done, 2, 3, 4, 5)
+
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(done, in)
+	c2 := sq(done, in)
+
+	// Consume the first value from output.
+	out := merge(done, c1, c2)
+	fmt.Println(<-out) // 4 or 9
+}
+
+func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		defer wg.Done()
+		for n := range c {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func gen(done <-chan struct{}, nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(done <-chan struct{}, in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			select {
+			case out <- n * n:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
+}

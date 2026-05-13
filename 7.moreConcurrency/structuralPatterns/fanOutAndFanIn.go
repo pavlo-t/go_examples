f4c8d8dda223ce9387b00b957f736a7904cf944c
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2026-05-13 21:02:32 +0300

    fanOut and fanIn concurrency patterns

---
+++ 7.moreConcurrency/structuralPatterns/fanOutAndFanIn.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func fanOutGen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fanOutSq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fanOutMerge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	in := fanOutGen(1, 2, 3, 4, 5)
+
+	// Distribute sq work across 2 goroutines (fan-out)
+	c1 := fanOutSq(in)
+	c2 := fanOutSq(in)
+
+	// merge channels back into one channel (fan-in)
+	for n := range fanOutMerge(c1, c2) {
+		fmt.Println(n)
+	}
+}

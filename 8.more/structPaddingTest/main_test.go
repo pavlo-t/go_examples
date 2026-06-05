bf53c83ede0ee6345b55573674d82d8077fd59c4
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2026-06-05 20:00:53 +0300

    bench struct performance based on struct padding due to different field order

--- 8.more/structPaddingTest/main_test.go
+++ 8.more/structPaddingTest/main_test.go
@@ -1,6 +1,21 @@
 package main
 
-// go test -benchmem -bench=. -count=5 8.more/structPaddingTest/main_test.go
+// $ go test -benchmem -bench=. -count=5 8.more/structPaddingTest/main_test.go
+// goos: linux
+// goarch: amd64
+// cpu: 11th Gen Intel(R) Core(TM) i7-1185G7 @ 3.00GHz
+// BenchmarkBadStruct-8                6832            220887 ns/op         2400257 B/op          1 allocs/op
+// BenchmarkBadStruct-8                3891            264177 ns/op         2400258 B/op          1 allocs/op
+// BenchmarkBadStruct-8                4333            232759 ns/op         2400258 B/op          1 allocs/op
+// BenchmarkBadStruct-8                5976            220011 ns/op         2400259 B/op          1 allocs/op
+// BenchmarkBadStruct-8                5742            232769 ns/op         2400258 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               8974            141120 ns/op         1605637 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               8574            139439 ns/op         1605638 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               7201            146399 ns/op         1605638 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               8566            132990 ns/op         1605639 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               8626            135354 ns/op         1605639 B/op          1 allocs/op
+// PASS
+// ok      command-line-arguments  15.834s
 import (
 	"testing"
 )

a1df138f85d40fc7d8b517251510d5a9301eea9f
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2026-06-05 21:27:58 +0300

    struct padding test: add benchmarks without allocations

--- 8.more/structPaddingTest/main_test.go
+++ 8.more/structPaddingTest/main_test.go
@@ -4,28 +4,28 @@ package main
 // goos: linux
 // goarch: amd64
 // cpu: 11th Gen Intel(R) Core(TM) i7-1185G7 @ 3.00GHz
-// BenchmarkBadStruct-8                6832            220887 ns/op         2400257 B/op          1 allocs/op
-// BenchmarkBadStruct-8                3891            264177 ns/op         2400258 B/op          1 allocs/op
-// BenchmarkBadStruct-8                4333            232759 ns/op         2400258 B/op          1 allocs/op
-// BenchmarkBadStruct-8                5976            220011 ns/op         2400259 B/op          1 allocs/op
-// BenchmarkBadStruct-8                5742            232769 ns/op         2400258 B/op          1 allocs/op
-// BenchmarkGoodStruct-8               8974            141120 ns/op         1605637 B/op          1 allocs/op
-// BenchmarkGoodStruct-8               8574            139439 ns/op         1605638 B/op          1 allocs/op
-// BenchmarkGoodStruct-8               7201            146399 ns/op         1605638 B/op          1 allocs/op
-// BenchmarkGoodStruct-8               8566            132990 ns/op         1605639 B/op          1 allocs/op
-// BenchmarkGoodStruct-8               8626            135354 ns/op         1605639 B/op          1 allocs/op
-// BenchmarkBadStructNoAlloc-8        16227             73360 ns/op             147 B/op          0 allocs/op
-// BenchmarkBadStructNoAlloc-8        16396             72669 ns/op             146 B/op          0 allocs/op
-// BenchmarkBadStructNoAlloc-8        16567             72363 ns/op             144 B/op          0 allocs/op
-// BenchmarkBadStructNoAlloc-8        16537             72414 ns/op             145 B/op          0 allocs/op
-// BenchmarkBadStructNoAlloc-8        16376             72885 ns/op             146 B/op          0 allocs/op
-// BenchmarkGoodStructNoAlloc-8       19412             61434 ns/op              82 B/op          0 allocs/op
-// BenchmarkGoodStructNoAlloc-8       19485             62043 ns/op              82 B/op          0 allocs/op
-// BenchmarkGoodStructNoAlloc-8       19420             60199 ns/op              82 B/op          0 allocs/op
-// BenchmarkGoodStructNoAlloc-8       19279             65251 ns/op              83 B/op          0 allocs/op
-// BenchmarkGoodStructNoAlloc-8       18819             67358 ns/op              85 B/op          0 allocs/op
+// BenchmarkBadStruct-8                6433            215287 ns/op         2400258 B/op          1 allocs/op
+// BenchmarkBadStruct-8                6214            181616 ns/op         2400257 B/op          1 allocs/op
+// BenchmarkBadStruct-8                5625            194506 ns/op         2400257 B/op          1 allocs/op
+// BenchmarkBadStruct-8                6594            191925 ns/op         2400257 B/op          1 allocs/op
+// BenchmarkBadStruct-8                6630            185213 ns/op         2400257 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               8433            123712 ns/op         1605635 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               9664            122348 ns/op         1605635 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               9788            128095 ns/op         1605635 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               9193            125675 ns/op         1605635 B/op          1 allocs/op
+// BenchmarkGoodStruct-8               9768            124760 ns/op         1605636 B/op          1 allocs/op
+// BenchmarkBadStructNoAlloc-8        16142             80697 ns/op             148 B/op          0 allocs/op
+// BenchmarkBadStructNoAlloc-8        15387             75834 ns/op             155 B/op          0 allocs/op
+// BenchmarkBadStructNoAlloc-8        13615             75204 ns/op             176 B/op          0 allocs/op
+// BenchmarkBadStructNoAlloc-8        15715             73439 ns/op             152 B/op          0 allocs/op
+// BenchmarkBadStructNoAlloc-8        16353             74815 ns/op             146 B/op          0 allocs/op
+// BenchmarkGoodStructNoAlloc-8       19194             61902 ns/op              83 B/op          0 allocs/op
+// BenchmarkGoodStructNoAlloc-8       19387             61776 ns/op              82 B/op          0 allocs/op
+// BenchmarkGoodStructNoAlloc-8       19545             61578 ns/op              82 B/op          0 allocs/op
+// BenchmarkGoodStructNoAlloc-8       18829             70859 ns/op              85 B/op          0 allocs/op
+// BenchmarkGoodStructNoAlloc-8       19213             63179 ns/op              83 B/op          0 allocs/op
 // PASS
-// ok      command-line-arguments  15.834s
+// ok      command-line-arguments  35.327s
 import (
 	"testing"
 )
@@ -52,7 +52,7 @@ func BenchmarkBadStruct(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		s := make([]Bad, 100_000)
 		for j := range s {
-			s[j].b = j
+			s[j].b = i
 		}
 		SinkBad = s
 	}
@@ -62,7 +62,7 @@ func BenchmarkGoodStruct(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		s := make([]Good, 100_000)
 		for j := range s {
-			s[j].b = j
+			s[j].b = i
 		}
 		SinkGood = s
 	}
@@ -73,7 +73,7 @@ func BenchmarkBadStructNoAlloc(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for j := range s {
 			s[j].a = !s[j].a
-			s[j].b = j
+			s[j].b = i
 			s[j].c = !s[j].c
 		}
 		SinkBad = s
@@ -85,7 +85,7 @@ func BenchmarkGoodStructNoAlloc(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for j := range s {
 			s[j].a = !s[j].a
-			s[j].b = j
+			s[j].b = i
 			s[j].c = !s[j].c
 		}
 		SinkGood = s
